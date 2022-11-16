0d8f512ab06d1cc4f979dec89864ad6c23b7fe15
Travis Parks <travisparks0788@gmail.com>
2022-11-16 15:51:46 -0500

    Add new string variables

--- variables.js
+++ variables.js
@@ -2,3 +2,6 @@ var places = ["New Zealand", "Tokyo", "Scotland"]
 var distance = [1301, 1502, 1800, 3]
 var like = [false, true, false, true]
 
+places = ["mexico", "taôs", "new york"]
+
+console.log(places)
